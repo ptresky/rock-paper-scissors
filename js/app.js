126366f675b87f25a93f1b61daed778943a370aa
ptresky <peter_tresky@hotmail.com>
2024-07-25 18:51:41 -0700

    add state variables and choices

---
+++ js/app.js
@@ -0,0 +1,34 @@
+/*-------------------------------- Constants --------------------------------*/
+// 3 choices of tock paper scissors
+// Dom 
+const choices = ['rock', 'paper', 'scissors'];
+
+
+
+/*-------------------------------- Constants --------------------------------*/
+
+const choices = ['rock', 'paper', 'scissors'];
+
+/*-------------------------------- Variables --------------------------------*/
+
+let playerChoice;
+let computerChoice; 
+let msg;
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+const resultDisplayEl = document.querySelector('#result-display');
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+//handle player click
+//generation of random selections for the comp player
+//compare player and comp choices to check for winner using if/else
+//render win/lose/tie messages
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+// handle a player clicking a button
